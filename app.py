c80f441149a346cd666eb882cf287c3279f9e91c
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-29 17:07:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from routes.alns_routes import alns_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(alns_bp, url_prefix="/alns")
+
+    @app.route("/", methods=["GET"])
+    def root():
+        return redirect(url_for("alns.index"))  # /alns/
+
     return app
 
 if __name__ == "__main__":
